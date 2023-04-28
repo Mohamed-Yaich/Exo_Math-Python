2b8c7aa0e37fe72e15fbe0a52dec7f7aff4ce6d6
Mohamed-Yaich <momoyaich75@gmail.com>
2023-04-28 14:24:39 +0200

    exo math

--- Integral_Rienman/exo.py
+++ Integral_Rienman/exo.py
@@ -12,13 +12,17 @@ step = (b-a)/nb_points
 
 x_values, y_values = [], []
 
+# 1
 x_values = numpy.linspace(a, b, nb_points)
 y_values = f(x_values)
 for index_point in range(0, nb_points+1):
     numpy.append(x_values, a + index_point*step)
     numpy.append(y_values, f(a + index_point*step))
+
+# 2
 gradient = numpy.gradient(y_values, x_values)
 
+# 3 
 y_slice = slice(3000, 7000)
 # print(y_values[y_slice])
 sum_y_slice = sum(y_values[y_slice])/len(y_values[y_slice])
@@ -27,6 +31,7 @@ sum_y_slice = sum(y_values[y_slice])/len(y_values[y_slice])
 std_dev = numpy.std(y_values[y_slice])
 # print(std_dev)
 
+# 4
 
 def f_x(x) :
     return ((numpy.exp(-x/10.0) + numpy.cos(x)) - ((0.1 * numpy.exp(-x/10.0)) + numpy.sin(x)))
